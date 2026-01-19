20ee1b23666249a1b45391cba7ca94207dd929f9
mbaransglm-boop <mbaransglm@gmail.com>
2026-01-19 05:06:55 +0300

    Update yeni.py

--- yeni.py
+++ yeni.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="SİSTEM HATASI", layout="centered")
 # Karanlık Tema
 st.markdown("<style>.main { background-color: #000000; color: #ff0000; }</style>", unsafe_allow_html=True)
 
-st.title("⚠️ KRİTİK HATA")
+st.title("⚠️ VİRÜS BULAŞIYOR")
 st.write("Sistem dosyaları siliniyor... Lütfen bekleyin.")
 
 # 20 Saniyelik Sayaç
@@ -16,5 +16,5 @@ for i in range(100):
     time.sleep(0.2)
     bar.progress(i + 1)
 
-st.error("DOSYALAR SİLİNDİ! Şaka yaptım, Mahvedeb47 gururla sunar. 😉")
+st.error("DOSYALAR SİLİNDİ! Şaka yaptım, SAPLAYICI gururla sunar. 😉")
 st.balloons()
